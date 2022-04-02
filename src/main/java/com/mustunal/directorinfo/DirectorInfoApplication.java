6c628c9c33283d97f0b09c784bf005356d49ed2f
mustunal <murat.ustunal@gmail.com>
2022-04-02 14:15:57 +0300

    Initial Commit

---
+++ src/main/java/com/mustunal/directorinfo/DirectorInfoApplication.java
@@ -0,0 +1,13 @@
+package com.mustunal.directorinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DirectorInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DirectorInfoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mustunal/directorinfo/DirectorInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mustunal.directorinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DirectorInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
